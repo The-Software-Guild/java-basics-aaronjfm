4ef3027cb7faf7c37bbb9f08a628db44c78e0911
aaronjfm <afm64@bath.ac.uk>
2022-04-21 22:03:30 +0100

    Split the game into a display class and the core gameplay functionality class.  refactored code to use OOP fundamentals such as encapsulation.

---
+++ display.java
@@ -0,0 +1,83 @@
+import java.util.*;
+public class display {
+
+    public static void main(String[] args) {
+        displayGame();
+    }
+
+    public static void displayGame() {
+        // Select Number of rounds between 1-10
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select number of rounds you wish to play between 1-10");
+        int numberOfRounds = sc.nextInt();
+        if(numberOfRounds < 1 || numberOfRounds > 10){
+            System.out.println("Incorrect number of rounds selected");
+            displayGame();
+        }
+        //loop
+        while (numberOfRounds > 0) {
+
+            // Ask Player to pick Rock Paper or Scissors
+            System.out.println("Press 1 for Rock, 2 for Paper, and 3 for Scissors");
+            int playerOption = sc.nextInt();
+            System.out.println("Player Selected " + numberToOption(playerOption));
+            // Computer Selects Random Option
+            int computerOption = (int) (Math.random() * (3 - 1)) + 1;
+            System.out.println("Computer Selected " + numberToOption(computerOption));
+
+            gameplay.winResult(playerOption, computerOption);
+
+            numberOfRounds--;
+        }
+        results();
+        playAgain();
+    }
+
+
+    public static void results(){
+        System.out.println("\n \nThe Results are:");
+        System.out.println("Player Wins = " + gameplay.playerWins);
+        System.out.println("Ties = " + gameplay.ties);
+        System.out.println("Computer Wins = " + gameplay.computerWins);
+
+        if(gameplay.playerWins > gameplay.computerWins){
+            System.out.println("The overall Winner is Human Player");
+        }
+        else if (gameplay.playerWins < gameplay.computerWins){
+            System.out.println("The overall Winner is Computer");
+        }
+        else{
+            System.out.println("Game ended a Draw");
+        }
+    }
+
+    public static String numberToOption(int gameNumber){
+        if(gameNumber == 1){
+            return "Rock";
+        }
+        else if(gameNumber == 2){
+            return "Paper";
+        }
+        else {
+            return "Scissors";
+        }
+    }
+
+    public static void playAgain(){
+        // Does the user want to play again?
+        System.out.println("\nDo you want to play again? Please type Yes or No");
+        Scanner sc = new Scanner(System.in);
+        String playAgain = sc.nextLine();
+        if(playAgain.equals("Yes")){
+            System.out.println("Starting New Game");
+            gameplay.resetWins();
+            displayGame();
+        }
+        else if(playAgain.equals("No")){
+            System.out.println("Game Over, Thank you for playing");
+        }
+        else{
+            System.out.println("Incorrect command entered, Thank you for playing");
+        }
+    }
+}
---
+++ gameplay.java
@@ -0,0 +1,58 @@
+public class gameplay {
+
+    static int playerWins = 0;
+    static int ties = 0;
+    static int computerWins = 0;
+
+    public static void winResult(int playerOption, int computerOption){
+        // Count Number of Player Wins, Ties, and Computer Wins
+
+        // Check win conditions
+        if(winConditions(playerOption, computerOption).equals("Draw")){
+            System.out.println("Draw");
+            ties++;
+        }
+        else if (winConditions(playerOption, computerOption).equals("Player Win")){
+            System.out.println("Player Win");
+            playerWins++;
+        }
+        else if (winConditions(playerOption, computerOption).equals("Computer Win")){
+            System.out.println("Computer Win");
+            computerWins++;
+        }
+    }
+
+    public static String winConditions(int playerOption, int computerOption) {
+        // Win Condition Applied
+        if (playerOption == computerOption) {
+            // Print results and declare overall winner
+            return "Draw";
+        }
+        else if (playerOption == 1 && computerOption == 2){
+            return "Computer Win";
+        }
+        else if (playerOption == 1 && computerOption == 3){
+            return "Player Win";
+        }
+        else if (playerOption == 2 && computerOption == 1){
+            return "Player Win";
+        }
+        else if (playerOption == 2 && computerOption == 3){
+            return "Computer Win";
+        }
+        else if (playerOption == 3 && computerOption == 1){
+            return "Computer Win";
+        }
+        else if (playerOption == 3 && computerOption == 2){
+            return "Player Win";
+        }
+        return "";
+    }
+
+    public static void resetWins(){
+        playerWins = 0;
+        ties = 0;
+        computerWins = 0;
+    }
+
+}
