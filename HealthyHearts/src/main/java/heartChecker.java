38dce8378a39d960bf5d759466a8d162b3893cbc
aaronjfm <afm64@bath.ac.uk>
2022-04-22 12:07:23 +0100

    Healthy Hearts Code

---
+++ HealthyHearts/src/main/java/heartChecker.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class heartChecker {
+
+    public static void main(String[] args) {
+        checkHeart();
+    }
+
+    public static void checkHeart(){
+        int HeartRate = 220;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int userAge = sc.nextInt();
+        int maxHeartRate = HeartRate - userAge;
+        int lowHeartRateRange = (maxHeartRate/100)*50;
+        int highHeartRateRange = (maxHeartRate/100)*85;
+        System.out.println("Your maximum heart rate should be " + maxHeartRate + " beats per minute");
+        System.out.println("Your target HR Zone is " + lowHeartRateRange + " - " + highHeartRateRange + " beats per minute");
+    }
+
+}
